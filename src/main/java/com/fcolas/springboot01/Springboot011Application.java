b9cbf01884a3dc9fcb62fa3360a822b8d1fb08ef
Fabrice Colas <fabrice_colas_78@msn.com>
2018-02-04 00:55:47 +0100

    First commit

---
+++ src/main/java/com/fcolas/springboot01/Springboot011Application.java
@@ -0,0 +1,12 @@
+package com.fcolas.springboot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot011Application.class, args);
+	}
+}
